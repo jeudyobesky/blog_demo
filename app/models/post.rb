c2d8b656ac66910252311624d0459c7411e29d7e
jeudyobesky <jeudy.obesk@gmail;com>
2023-11-16 15:47:01 +0100

    Add devise users, user name in sessions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: {minimum:5, maximum:15}
   validates :body, presence: true, length: { maximum:180 }
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create(email: "john@gmail.com", password: "azerty", password_confirmation: "azerty")
+
 10.times do |x|
   Post.create!(
     title: "Titre n : #{x + 1}",
-    body: "#{x+1}"
+    body: "#{x+1}",
+    user_id: User.first.id
   )
 end
